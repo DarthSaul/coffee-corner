ef0f5a4c85a31594300f82b450e057519f1eb569
Saul Graves <graves.saul@gmail.com>
2021-06-11 16:13:38 -0400

    Init frontend react app

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Switch, Route, Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+    return (
+        <div className='App container py-5'>
+            <h1 className='display-1'>Hello, world</h1>
+        </div>
+    );
+}
+
+export default App;
