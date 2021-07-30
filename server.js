de94679f6c7adf84b13d96afd7bd55a2393d4fb6
Saul Graves <graves.saul@gmail.com>
2021-07-30 13:02:14 -0500

    clean

--- server.js
+++ server.js
@@ -57,15 +57,5 @@ app.use('*', (req, res) => {
     res.status(404).json({ error: 'not found' });
 });
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
-
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
-}
-
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
-
-// export default app;
