22c1b7288487f5cb97bf6b1c45d55de559d9f304
Saul Graves <graves.saul@gmail.com>
2021-08-27 11:58:44 -0400

    cleaning

--- client/src/http-common.js
+++ client/src/http-common.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-    // baseURL: process.env.baseURL || 'http://localhost:5000/api/v1',
     baseURL: '/api/v1',
     header: {
         'Content-type': 'application/json'
